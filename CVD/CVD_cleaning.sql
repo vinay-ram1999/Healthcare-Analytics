f56b9948fcaf03994faead4fcfab9514894cd864
Vinay Ram Gazula <vinayramgazula@gmail.com>
2024-08-25 21:06:59 -0400

    data cleaning completed;

--- CVD/CVD_cleaning.sql
+++ CVD/CVD_cleaning.sql
@@ -35,7 +35,7 @@ CREATE TABLE CVD (
     question_id VARCHAR(10),
     data_value_type_id VARCHAR(10),
     break_out_category_id VARCHAR(10),
-    breat_out_id VARCHAR(10),
+    break_out_id VARCHAR(10),
     location_id VARCHAR(10),
     geo_location TEXT
 );
@@ -79,12 +79,120 @@ SET row_id = IF(@1 = '', NULL, @1),
     question_id = IF(@25 = '', NULL, @25),
     data_value_type_id = IF(@26 = '', NULL, @26),
     break_out_category_id = IF(@27 = '', NULL, @27),
-    breat_out_id = IF(@28 = '', NULL, @28),
+    break_out_id = IF(@28 = '', NULL, @28),
     location_id = IF(@29 = '', NULL, @29),
     geo_location = IF(@30 = '', NULL, @30);
 
 
+-- Data Cleaning --
 
+-- Before manuplating the data, create a staging or copy of original data
+DROP TABLE IF EXISTS stagging_CVD;
+CREATE TABLE stagging_CVD
+LIKE CVD;
+
+INSERT INTO stagging_CVD
 SELECT * FROM CVD;
 
+-- Drop unwanted columns
+SELECT DISTINCT priority_area_1 FROM stagging_CVD; -- remove
 
+SELECT DISTINCT priority_area_2 FROM stagging_CVD; -- remove
+
+SELECT DISTINCT priority_area_3 FROM stagging_CVD;
+
+SELECT priority_area_3, COUNT(*)
+FROM stagging_CVD
+GROUP BY priority_area_3;
+
+SELECT DISTINCT priority_area_4 FROM stagging_CVD; -- remove
+
+SELECT DISTINCT cls FROM stagging_CVD; -- remove (generalize in a table before removing)
+
+SELECT DISTINCT topic FROM stagging_CVD; -- remove (generalize in a table before removing)
+
+SELECT DISTINCT question FROM stagging_CVD; -- remove (generalize in a table before removing)
+
+SELECT DISTINCT break_out_category FROM stagging_CVD; -- remove (generalize in a table before removing)
+
+SELECT DISTINCT break_out FROM stagging_CVD; -- remove (generalize in a table before removing)
+
+SELECT DISTINCT data_value_unit FROM stagging_CVD; -- remove since the unit is 'Rate per 100,000' constant for all records
+
+SELECT DISTINCT cls_id FROM stagging_CVD; -- generalize using these values
+
+SELECT DISTINCT topic_id FROM stagging_CVD; -- generalize using these values
+
+SELECT DISTINCT break_out_category_id FROM stagging_CVD; -- generalize using these values
+
+SELECT DISTINCT break_out_id FROM stagging_CVD; -- generalize using these values
+
+SELECT DISTINCT location_id FROM stagging_CVD; -- remove
+
+-- Generalizing the data using the id's
+SELECT location_abbr, location_desc, location_id
+FROM CVD
+GROUP BY location_abbr, location_desc, location_id;
+
+SELECT cls, cls_id
+FROM CVD
+GROUP BY cls, cls_id;
+
+SELECT topic, topic_id
+FROM CVD
+GROUP BY topic, topic_id;
+
+SELECT question, question_id
+FROM CVD
+GROUP BY question, question_id;
+
+SELECT data_value_type, data_value_type_id
+FROM CVD
+GROUP BY data_value_type, data_value_type_id;
+
+SELECT break_out_category, break_out_category_id
+FROM CVD
+GROUP BY break_out_category, break_out_category_id;
+
+SELECT break_out, break_out_id
+FROM CVD
+GROUP BY break_out, break_out_id;
+
+
+SELECT COUNT(*) 
+FROM stagging_CVD
+WHERE data_value_alt != data_value AND data_value IS NOT NULL AND data_value_alt IS NOT NULL;
+-- remove data_value_alt since both data_value_alt and data_value are same in all cases
+
+SELECT COUNT(*) FROM (SELECT DISTINCT geo_location FROM stagging_CVD WHERE geo_location IS NOT NULL) AS geo;
+-- there are only 51 distinct geo_locations pointing out that they are indicating location of each state so we can remove it
+
+-- drop rows with data_value_footnote_symbol = '~' which means 'Statistically unstable estimates not presented [unstable by NCHS standards: (standard error/estimate>0.23 or numerator <20)]'
+DELETE FROM stagging_CVD
+WHERE data_value_footnote_symbol = '~';
+-- now we can delete columns data_value_footnote_symbol and data_value_footnote as they are no longer needed
+
+-- Drop columns from stagging_CVD
+ALTER TABLE stagging_CVD
+DROP COLUMN row_id,
+DROP COLUMN location_abbr,
+DROP COLUMN data_source,
+DROP COLUMN priority_area_1,
+DROP COLUMN priority_area_2,
+DROP COLUMN priority_area_4,
+DROP COLUMN cls,
+DROP COLUMN topic,
+DROP COLUMN question,
+DROP COLUMN data_value_type,
+DROP COLUMN data_value_unit,
+DROP COLUMN data_value_alt,
+DROP COLUMN data_value_footnote_symbol,
+DROP COLUMN data_value_footnote,
+DROP COLUMN break_out_category,
+DROP COLUMN break_out,
+DROP COLUMN location_id,
+DROP COLUMN geo_location;
+
+SELECT * FROM stagging_CVD;
+
+SELECT COUNT(*) FROM stagging_CVD;
